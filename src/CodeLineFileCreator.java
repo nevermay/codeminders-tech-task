6f81dba71dc7a75f9c2e3af87ad3572243b077fa
Pavlo Yatsura <pavloyatsura@Pavlos-MacBook.local>
2020-03-11 01:59:20 +0200

    Adds condition to evaluate single file

--- src/CodeLineFileCreator.java
+++ src/CodeLineFileCreator.java
@@ -12,30 +12,37 @@ public final class CodeLineFileCreator {
 
     public static CodeLineFile build(final Path rootPath, final CodeLineFile root)
             throws IOException {
-        return buildWithIndent(rootPath, root, EMPTY);
+        if (Files.isRegularFile(rootPath)) {
+            return buildSingleFile(rootPath, EMPTY);
+        } else {
+            return buildWithIndent(rootPath, root, EMPTY);
+        }
     }
 
     private static CodeLineFile buildWithIndent(final Path rootPath, final CodeLineFile root, final String indent)
             throws IOException {
         final String tab = indent + EMPTY_SPACE;
-        for (final Path fileEntry : Files.list(rootPath).collect(Collectors.toList())) {
-            if (Files.isDirectory(fileEntry)) {
-                final String tabulatedName = tab + fileEntry.toFile().getName();
+        for (final Path childPath : Files.list(rootPath).collect(Collectors.toList())) {
+            if (Files.isDirectory(childPath)) {
+                final String tabulatedName = tab + childPath.toFile().getName();
                 final CodeLineFile childDirectory = new CodeLineFile(tabulatedName);
                 root.addChild(childDirectory);
 
-                buildWithIndent(fileEntry, childDirectory, tab);
+                buildWithIndent(childPath, childDirectory, tab);
             } else {
-                final List<String> lines = Files.readAllLines(fileEntry, StandardCharsets.UTF_8);
-                final int linesOfCode = CodeLineCounter.count(lines);
-
-                final String tabulatedName = tab + fileEntry.toFile().getName();
-                final CodeLineFile childFile = new CodeLineFile(tabulatedName, linesOfCode);
-
+                final CodeLineFile childFile = buildSingleFile(childPath, tab);
                 root.addChild(childFile);
             }
         }
 
         return root;
+    }
+
+    private static CodeLineFile buildSingleFile(final Path filePath, final String tab) throws IOException {
+        final List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+        final int linesOfCode = CodeLineCounter.count(lines);
+
+        final String tabulatedName = tab + filePath.toFile().getName();
+        return new CodeLineFile(tabulatedName, linesOfCode);
     }
 }
